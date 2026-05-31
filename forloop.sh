8843a207d86ee010a6f514c0b5f28dc4a07cc92a
AadarshKumarAI <aadarshkumardream@gmail.com>
2026-05-31 12:33:02 +0000

    adding ab1.txt file

--- forloop.sh
+++ forloop.sh
@@ -8,7 +8,7 @@ task
 
 for (( i=$2 ; i<=$3; i++ )); 
 do
-	touch $1$2.txt && git add . && git commit -m "adding $1$2.txt file" 
+	touch $1$2 && git add . && git commit -m "adding $1$2.txt file" 
 done
 
 git push -uf origin main 
